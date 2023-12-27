00db26045f838bcd9dbd056dc8fcc9b6a59f0d9e
izzat <izzat@gmail.com>
2023-12-27 17:49:59 +0500

    all codes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+    const text = "lorem ipsum";
+
+    function testFunction() {
+        console.log("function is working!!!");
+    }
+    return (
+        <div className='container'>
+            <button className='btn'>click</button>
+            <span></span>
+        </div>
+    );
+}
+
+export default App;
